ff3e175c6f7bbd45225a2c74160b302c1deb29a6
Ehua <ehua999@163.com>
2022-01-04 08:18:39 +0800

    更新

---
+++ src/Layuimini/Layuimini.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Ehua\Layuimini;
+
+
+class Layuimini
+{
+
+    /**
+     * 构造request 中的json参数  返回数组格式 直接进行使用
+     * @param $key      json key
+     * @return array|mixed|null
+     */
+    static function request_json($key)
+    {
+        $temp = request()->param($key);
+        $temp = json_decode($temp, true);
+        return array_filter($temp);
+    }
+}
