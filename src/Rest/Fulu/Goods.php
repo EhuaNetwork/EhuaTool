ea99c6ad4540d8f0cab32aaa7ac4d04a757c1b6e
zhaoyi8841 <2084364531.qq.com>
2022-07-05 13:22:25 +0800

    福禄API

--- src/Rest/Fulu/Goods.php
+++ src/Rest/Fulu/Goods.php
@@ -6,5 +6,103 @@ namespace Ehua\Rest\Fulu;
 
 class Goods extends Common
 {
+    /**
+     * fulu.goods.template.get 获取商品模板接口在线调试（沙箱环境）
+     * 1、直充商品必须调用商品模板，卡密商品无需调用商品模板；
+     *
+     * 2、获取商品模板信息（请求参数template_id，通过商品接口获得）；
+     *
+     * POST请求，Content-Type必须设置为：application/json；
+     *
+     * 3、商品模板接口对接步骤： 3.1、通过商品编号，调用“获取商品信息接口”，获取商品模板编号；
+     *
+     * 3.2、通过商品模板编号，调用“获取商品模板接口”，获取商品模板信息；
+     *
+     * 3.3、商品模板是json格式字符串，需要渲染到前端，用户充值时，由用户自行选择游戏区服、充值方式等信息；
+     * @param $id
+     * @return bool|string
+     */
+    public function template($id)
+    {
+        $biz_content = [
+            'template_id' => $id,//商品模板编号
+        ];
+
+        $data = [
+            'app_key' => $this->app_key,
+            'method' => 'fulu.goods.template.get',
+            'timestamp' => date('Y-m-d H:i:s', time()),
+            'version' => '2.0',
+            'format' => 'json',
+            'charset' => 'utf-8',
+            'sign_type' => 'md5',
+            'app_auth_token' => '',
+            'biz_content' => json_encode($biz_content),
+        ];
+        $data['sign'] = $this->getSign($data);
+        return $this->http($data);
+    }
+
+    /**
+     * fulu.goods.list.get 获取商品列表接口在线调试（沙箱环境）
+     * 获取商品列表接口；
+     *
+     * POST请求，Content-Type必须设置为：application/json；
+     */
+    public function list()
+    {
+        $biz_content = [
+//                'first_category_id'=>'219',//商品分类Id（一级）（预留参数，暂无商品分类接口提供）
+//                'second_category_id'=>'708',//商品分类Id（二级）（预留参数，暂无商品分类接口提供）
+//                'third_category_id'=>'709',//商品分类Id（三级）（预留参数，暂无商品分类接口提供）
+//                'product_id'=>'10000001',//商品编号
+//                'product_name'=>'ww腾讯Q币直充一元',//商品名称
+//                'product_type'=>'直充',//库存类型：卡密、直充
+            'face_value' => '100',
+        ];
+
+        $data = [
+            'app_key' => $this->app_key,
+            'method' => 'fulu.goods.list.get',
+            'timestamp' => date('Y-m-d H:i:s', time()),
+            'version' => '2.0',
+            'format' => 'json',
+            'charset' => 'utf-8',
+            'sign_type' => 'md5',
+            'app_auth_token' => '',
+            'biz_content' => json_encode($biz_content),
+        ];
+        $data['sign'] = $this->getSign($data);
+        return $this->http($data);
+    }
+
+
+    /**
+     * fulu.goods.info.get 获取商品信息接口在线调试（沙箱环境）
+     * 获取商品信息接口；
+     *
+     * POST请求，Content-Type必须设置为：application/json；
+     */
+    public function info($id)
+    {
+        $biz_content = [
+            'product_id' => $id,
+        ];
+
+        $data = [
+            'app_key' => $this->app_key,
+            'method' => 'fulu.goods.info.get',
+            'timestamp' => date('Y-m-d H:i:s', time()),
+            'version' => '2.0',
+            'format' => 'json',
+            'charset' => 'utf-8',
+            'sign_type' => 'md5',
+            'app_auth_token' => '',
+            'biz_content' => json_encode($biz_content),
+        ];
+        $data['sign'] = $this->getSign($data);
+        return $this->http($data);
+    }
+
 
 }
--- src/Rest/Fulu/Order.php
+++ src/Rest/Fulu/Order.php
@@ -6,21 +6,171 @@ namespace Ehua\Rest\Fulu;
 
 class Order extends Common
 {
-    public function mobile()
+    /**
+     * fulu.order.mobile.add 话费下单接口在线调试（沙箱环境）
+     * 接口介绍：话费商品下单接口，供合作方针对话费类型商品进行下单；
+     *
+     * 1、POST请求，Content-Type必须设置为：application/json；
+     *
+     * 2、接口是异步，接口调用成功(即下单成功)，不代表充值成功，最终“充值结果”，需要调用“订单查询接口”进行查询，由于是异步操作，建议间隔1-3s循环调用，直至最终结果；
+     *
+     * 3、“订单查询接口”必须接入；
+     */
+    public function mobile($phone, $money)
     {
-        $config=[
-            'app_key'=>'eP7b5DEShVbohUYkgQA3wiFco6Y1bIHk/b7+dMeMJs0FXogJv6ijN0vbNcYFLvRX',
-            'token'=>'25c543ef40e24907a10882ccbb784cca',
+
+        $biz_content = [
+            'charge_phone' => $phone,//充值手机号
+            'charge_value' => $money,//充值数额
+            'customer_order_no' => uniqid(),//外部订单号
+            'customer_price' => $money,//外部销售价
+            'shop_type' => '系统',//店铺类型（PDD、淘宝、天猫、京东、苏宁、其他）；非必填字段，可忽略
+            'external_biz_id' => 'C564982164',//透传字段
         ];
 
-        (new \Ehua\Rest\Fulu\Order(false,$config))->mobile();
+        $data = [
+            'app_key' => $this->app_key,
+            'method' => 'fulu.order.mobile.add',
+            'timestamp' => date('Y-m-d H:i:s', time()),
+            'version' => '2.0',
+            'format' => 'json',
+            'charset' => 'utf-8',
+            'sign_type' => 'md5',
+            'app_auth_token' => '',
+            'biz_content' => json_encode($biz_content),
+        ];
+        $data['sign'] = $this->getSign($data);
+        return $this->http($data);
+    }
+
+
+    /**
+     * fulu.order.direct.add 直充下单接口在线调试（沙箱环境）
+     * 接口介绍：直充商品下单接口，供合作方针对直充类型商品进行下单；
+     *
+     * 1、POST请求，Content-Type必须设置为：application/json；
+     * 2、接口是异步，接口调用成功(即下单成功)，不代表充值成功，最终“充值结果”，需要调用“订单查询接口”进行查询，由于是异步操作，建议间隔1-3s循环调用，直至最终结果；
+     * 3、“订单查询接口”必须接入；
+     * 4、直充商品必须调用商品模板，卡密商品无需调用商品模板；
+     */
+    public function direct($data, $template)
+    {
+        //模板：ChargeAccount-----下单参数：charge_account
+        //模板：ChargeNum---------下单参数：buy_num
+        //模板：ChargeGame--------下单参数：charge_game_name
+        //模板：ChargeRegion------下单参数：charge_game_region
+        //模板：ChargeServer------下单参数：charge_game_srv
+        //模板：ChargeType--------下单参数：charge_type
+        //模板：ChargeIp----------下单参数：charge_ip
+        //模板：ChargePWD---------下单参数：charge_password
+        //模板：ContactQQ---------下单参数：contact_qq
+        //模板：ContactType-------下单参数：contact_tel
         $biz_content = [
-            'charge_phone' => 15725517053,
-            'charge_value' => 500,
-            'customer_order_no' => uniqid(),//外部订单号
-            'customer_price' => 500,//外部销售价
-            'shop_type' => '京东',
-            'external_biz_id' => 'C564982164',//透传字段
+            'product_id' => $data['product_id'],//商品编号
+            'customer_order_no' => $data['order_no'],//外部订单号
+
+            'charge_account' => $template['ChargeAccount'],//充值账号
+            'buy_num' => $template['ChargeNum'],//购买数量
+
+
+//            'remaining_number' => $data['product_id'],//剩余数量
+//            'charge_game_role' => $data['product_id'],//充值游戏角色
+//            'customer_price' => $data['product_id'],//外部销售价
+//            'shop_type' => $data['product_id'],//店铺类型（PDD、淘宝、天猫、京东、苏宁、其他；非必填字段，可忽略
+            'external_biz_id' => null,//透传字段
+        ];
+
+
+        if (isset($template['ChargeGame'])) {//充值游戏名称
+            $biz_content['charge_game_name'] = $template['ChargeGame'];
+        }
+        if (isset($template['ChargeRegion'])) {//充值游戏区
+            $biz_content['charge_game_region'] = $template['ChargeRegion'];
+        }
+        if (isset($template['ChargeServer'])) {//充值游戏服
+            $biz_content['charge_game_srv'] = $template['ChargeServer'];
+        }
+        if (isset($template['ChargeType'])) {//充值类型
+            $biz_content['charge_type'] = $template['ChargeType'];
+        }
+        if (isset($template['ChargePWD'])) {//充值密码，部分游戏类要传
+            $biz_content['charge_password'] = $template['ChargePWD'];
+        }
+        if (isset($template['ChargeIp'])) {//下单真实Ip，区域商品要传
+            $biz_content['charge_ip'] = $template['ChargeIp'];
+        }
+        if (isset($template['ContactQQ'])) {//联系QQ
+            $biz_content['contact_qq'] = $template['ContactQQ'];
+        }
+        if (isset($template['ContactType'])) {//联系电话
+            $biz_content['contact_tel'] = $template['ContactType'];
+        }
+
+        $data = [
+            'app_key' => $this->app_key,
+            'method' => 'fulu.order.direct.add',
+            'timestamp' => date('Y-m-d H:i:s', time()),
+            'version' => '2.0',
+            'format' => 'json',
+            'charset' => 'utf-8',
+            'sign_type' => 'md5',
+            'app_auth_token' => '',
+            'biz_content' => json_encode($biz_content),
+        ];
+        $data['sign'] = $this->getSign($data);
+        return $this->http($data);
+    }
+
+    /**
+     * fulu.order.card.add 卡密下单接口在线调试（沙箱环境）
+     * 接口介绍：卡密商品取卡接口，供合作方针对卡密类型商品进行下单；
+     *
+     * 1、POST请求，Content-Type必须设置为：application/json；
+     *
+     * 2、接口是异步，接口调用成功(即下单成功)，不代表充值成功，最终“充值结果”，需要调用“订单查询接口”进行查询，由于取卡是异步操作，建议间隔1-3s循环调用，直至最终结果；
+     *
+     * 3、此接口不会返回卡密数据，需要再调用“订单查询接口”获取卡密信息；
+     *
+     * 4、“订单查询接口”必须接入；
+     */
+    public function card($data,$template)
+    {
+        $biz_content = [
+            'product_id' => $data['product_id'],//商品编号
+            'buy_num' => $template['ChargeNum'],//购买数量
+            'customer_order_no' => $data['order_no'],//外部订单号
+//            'customer_price' => $data['price'],//外部销售价
+//            'shop_type' =>  null,//店铺类型（PDD、淘宝、天猫、京东、苏宁、其他）；非必填字段，可忽略
+//            'external_biz_id' => null,//透传字段
+        ];
+        $data = [
+            'app_key' => $this->app_key,
+            'method' => 'fulu.order.card.add',
+            'timestamp' => date('Y-m-d H:i:s', time()),
+            'version' => '2.0',
+            'format' => 'json',
+            'charset' => 'utf-8',
+            'sign_type' => 'md5',
+            'app_auth_token' => '',
+            'biz_content' => json_encode($biz_content),
+        ];
+        $data['sign'] = $this->getSign($data);
+        return $this->http($data);
+    }
+
+
+    /**
+     * fulu.order.info.get 订单查询接口在线调试（沙箱环境）
+     * 接口介绍：查询订单详情接口，供合作方查询订单充值结果时调用；
+     *
+     * 1、POST请求，Content-Type必须设置为：application/json；
+     * 2、如果是卡密取卡订单，此接口会返回卡密数据；
+     * 3、卡密为密文，需要进行解密使用，详情请查看右侧常见问题->4.卡密解密说明；
+     */
+    public function info($order_no)
+    {
+        $biz_content = [
+            'customer_order_no' => $order_no,//外部订单号
         ];
         $data = [
             'app_key' => $this->app_key,
@@ -33,9 +183,11 @@ class Order extends Common
             'app_auth_token' => '',
             'biz_content' => json_encode($biz_content),
         ];
-
         $data['sign'] = $this->getSign($data);
-
-        $this->http($data);
+        return $this->http($data);
     }
+
+
+
+
 }
